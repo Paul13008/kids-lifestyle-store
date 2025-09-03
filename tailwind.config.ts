d4ce046afd668ac76ab315b5b5b05e8383e94430
Paul13008 <paul.lafon13@gmail.com>
2025-09-03 18:47:59 +0200

    chore(css): add tailwind/postcss/autoprefixer config

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './styles/**/*.{css}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
